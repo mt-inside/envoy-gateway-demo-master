7bdd81d7786429fc7d87ce01e461c36cf5d0bed4
Matt Turner <user.personal@users.noreply.github.com>
2022-09-28 16:22:22 -0400

    Docs & make it work

--- 02-install-gw-api.sh
+++ 02-install-gw-api.sh
@@ -1,4 +1,5 @@
 source 00-common.sh
 
+# NOTE: installs Pods (admission hook), not just CRDs
 kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/v0.2.0-rc1/gatewayapi-crds.yaml
 #kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v${GW_API_VERSION}/experimental-install.yaml
--- 06-expose-example-apps.sh
+++ 06-expose-example-apps.sh
@@ -3,20 +3,23 @@ source 00-common.sh
 # NB: Must (v 0.2.0) be applied in order, due to #301
 
 # GatewayClass
-kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/gatewayclass.yaml
-#kubectl apply -f 06/gatewayclass.yaml
+# - The physical gateway Pod is named after this (ie envoy-foo), but doesn't appear until gwclass, gw, and httproute have been deployed
+# - Must be called "eg" - tries to use an eponymous SA, but if it's not called eg, that sa (ie envoy-eg) simply isn't made
+#kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/gatewayclass.yaml
+kubectl apply -f 06/gatewayclass.yaml
 
 sleep 10
 
 # Gateway (port 8080)
-kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/gateway.yaml
-#kubectl apply -f 06/gateway.yaml
+#kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/gateway.yaml
+kubectl apply -f 06/gateway.yaml
 
 sleep 10
 
 # HTTPRoute
 #kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/httproute.yaml
-#kubectl apply -f 06/httpbin.yaml
+kubectl apply -f 06/httpbin.yaml
+
 kubectl apply -f 06/http-log.yaml
 
 watch -n1 kubectl -n envoy-gateway-system get pods
