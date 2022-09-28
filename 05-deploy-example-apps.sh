b131172e56587344303acb2cd3eb55cff92695f4
Matt Turner <user.personal@users.noreply.github.com>
2022-09-28 17:00:38 -0400

    Add option for shimming httpbin to http-log

--- 05-deploy-example-apps.sh
+++ 05-deploy-example-apps.sh
@@ -7,7 +7,8 @@ kubectl kustomize --load-restrictor=LoadRestrictionsNone 05/ | kubectl apply -f 
 
 # == httpbin ==
 
-# kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/httpbin.yaml
+kubectl apply -f https://raw.githubusercontent.com/envoyproxy/gateway/v${ENVOY_GW_VERSION}/examples/kubernetes/httpbin.yaml
+# kubectl apply -f 05a/httpbin-shim.yaml # Use this if httpbin isn't loading because the image is too big for shit wifi
 
 
 watch -n1 kubectl get pods
