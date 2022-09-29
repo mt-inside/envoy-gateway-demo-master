5cbc5c2f01bd633a7803b552fb63852ac2f37ff5
Matt Turner <user.personal@users.noreply.github.com>
2022-09-29 13:52:41 -0400

    Clarify where to get GW API CRDs from

--- 02-install-gw-api.sh
+++ 02-install-gw-api.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
 source 00-common.sh
 
+
 # NOTE: installs Pods (admission hook), not just CRDs
-kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/v0.2.0-rc1/gatewayapi-crds.yaml
-#kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v${GW_API_VERSION}/experimental-install.yaml
+#kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/v${ENVOY_GW_VERSION}/gatewayapi-crds.yaml
+# Last time I checked (29/9/22), ^^ was a vendor of upstream 0.5.0 experimental-install
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v${GW_API_VERSION}/experimental-install.yaml
+
+watch -n1 kubectl get pods --namespace gateway-system
