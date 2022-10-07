28349073231c037d5cb927b6b8e34a001902bd05
Matt Turner <user.personal@users.noreply.github.com>
2022-10-07 14:00:18 +0100

    Upgrade to 0.2.0-rc2: fixes a bug or two

--- 00-versions.sh
+++ 00-versions.sh
@@ -3,7 +3,7 @@ declare -r NAME=demo
 declare -r KUBERNETES_VERSION=v1.25.2
 
 declare -rx GW_API_VERSION=0.5.1
-declare -rx ENVOY_GW_VERSION=0.2.0-rc1
+declare -rx ENVOY_GW_VERSION=0.2.0-rc2
 
 declare -r CERT_MANAGER_VERSION=1.9.1
 
